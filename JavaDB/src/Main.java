fd9777370ded7efcce8421b074ff88988fcc5084
Suraj Naveen <suraj0nb@gmail.com>
2024-04-05 14:57:14 +0530

    Read data from database

--- JavaDB/src/Main.java
+++ JavaDB/src/Main.java
@@ -1,7 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Connection connection = null;
+        String query = "select * from basicdata;";
+        try{
+            CreateConnection link = new CreateConnection();
+            connection = link.setConnection();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
 
+            while (result.next()){
+                String name = result.getString(2);
+                String address = result.getString(3);
+
+                System.out.println("Name: "+ name + " Address: "+address);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Something went wrong");
+        }
     }
 }
